8163984469c98003fe63e839cd1985ed1c886525
Hezzekiarafael <hrafaelmiracle@gmail.com>
2026-05-04 17:40:51 +0700

    Update for uts

--- app/Http/Controllers/Dokter/JadwalPeriksaController.php
+++ app/Http/Controllers/Dokter/JadwalPeriksaController.php
@@ -11,7 +11,7 @@ class JadwalPeriksaController extends Controller
 {
     public function index(){
         $dokter = Auth::user();
-        $jadwalPeriksas = JadwalPeriksa::where('id_dokter', $dokter->id)->orderBy('hari')->get();
+        $jadwalPeriksas = JadwalPeriksa::query()->where('id_dokter', $dokter->id)->orderBy('hari')->get();
         return view('dokter.jadwal-periksa.index', compact('jadwalPeriksas'));
     }
 

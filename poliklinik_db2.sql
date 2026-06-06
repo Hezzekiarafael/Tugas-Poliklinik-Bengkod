743ed988981dbd562d9b323d28d1606eda651ba9
Hezzekiarafael <hrafaelmiracle@gmail.com>
2026-06-07 03:51:48 +0700

    Add database sql

--- poliklinik_db2.sql
+++ poliklinik_db2.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2026 at 02:29 PM
--- Server version: 8.4.3
+-- Generation Time: Jun 06, 2026 at 08:50 PM
+-- Server version: 10.4.32-MariaDB-log
 -- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cache` (
-  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `value` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `expiration` int NOT NULL
+  `key` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL,
+  `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -40,9 +40,9 @@ CREATE TABLE `cache` (
 --
 
 CREATE TABLE `cache_locks` (
-  `key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `owner` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `expiration` int NOT NULL
+  `key` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `expiration` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -52,14 +52,25 @@ CREATE TABLE `cache_locks` (
 --
 
 CREATE TABLE `daftar_poli` (
-  `id` bigint UNSIGNED NOT NULL,
-  `id_pasien` bigint UNSIGNED NOT NULL,
-  `id_jadwal` bigint UNSIGNED NOT NULL,
-  `keluhan` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `no_antrian` int NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_pasien` bigint(20) UNSIGNED NOT NULL,
+  `id_jadwal` bigint(20) UNSIGNED NOT NULL,
+  `keluhan` text NOT NULL,
+  `no_antrian` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `daftar_poli`
+--
+
+INSERT INTO `daftar_poli` (`id`, `id_pasien`, `id_jadwal`, `keluhan`, `no_antrian`, `created_at`, `updated_at`) VALUES
+(1, 6, 1, 'Mata sakit', 1, '2026-04-22 22:33:35', '2026-04-22 22:33:35'),
+(2, 6, 2, 'Mata saya masih rabun kak', 1, '2026-04-22 22:51:09', '2026-04-22 22:51:09'),
+(3, 4, 1, 'Mata Sakit', 2, '2026-05-04 01:08:39', '2026-05-04 01:08:39'),
+(4, 4, 1, 'Sakit mata', 3, '2026-06-06 12:11:56', '2026-06-06 12:11:56'),
+(5, 4, 2, 'sakit mata lagi', 2, '2026-06-06 12:26:18', '2026-06-06 12:26:18');
 
 -- --------------------------------------------------------
 
@@ -68,12 +79,20 @@ CREATE TABLE `daftar_poli` (
 --
 
 CREATE TABLE `detail_periksa` (
-  `id` bigint UNSIGNED NOT NULL,
-  `id_periksa` bigint UNSIGNED NOT NULL,
-  `id_obat` bigint UNSIGNED NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_periksa` bigint(20) UNSIGNED NOT NULL,
+  `id_obat` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `detail_periksa`
+--
+
+INSERT INTO `detail_periksa` (`id`, `id_periksa`, `id_obat`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2026-05-04 01:10:30', '2026-05-04 01:10:30'),
+(2, 2, 1, '2026-06-06 12:13:26', '2026-06-06 12:13:26');
 
 -- --------------------------------------------------------
 
@@ -82,13 +101,13 @@ CREATE TABLE `detail_periksa` (
 --
 
 CREATE TABLE `failed_jobs` (
-  `id` bigint UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -98,14 +117,22 @@ CREATE TABLE `failed_jobs` (
 --
 
 CREATE TABLE `jadwal_periksa` (
-  `id` bigint UNSIGNED NOT NULL,
-  `id_dokter` bigint UNSIGNED NOT NULL,
-  `hari` enum('Senin','Selasa','Kamis','Jumat','Sabtu','Minggu') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_dokter` bigint(20) UNSIGNED NOT NULL,
+  `hari` enum('Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu') NOT NULL,
   `jam_mulai` time NOT NULL,
   `jam_selesai` time NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `jadwal_periksa`
+--
+
+INSERT INTO `jadwal_periksa` (`id`, `id_dokter`, `hari`, `jam_mulai`, `jam_selesai`, `created_at`, `updated_at`) VALUES
+(1, 2, 'Selasa', '02:33:00', '03:45:00', '2026-04-20 00:05:10', '2026-04-20 00:05:10'),
+(2, 2, 'Rabu', '02:01:00', '03:00:00', '2026-04-22 22:05:43', '2026-04-22 22:05:43');
 
 -- --------------------------------------------------------
 
@@ -114,13 +141,13 @@ CREATE TABLE `jadwal_periksa` (
 --
 
 CREATE TABLE `jobs` (
-  `id` bigint UNSIGNED NOT NULL,
-  `queue` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `attempts` tinyint UNSIGNED NOT NULL,
-  `reserved_at` int UNSIGNED DEFAULT NULL,
-  `available_at` int UNSIGNED NOT NULL,
-  `created_at` int UNSIGNED NOT NULL
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `attempts` tinyint(3) UNSIGNED NOT NULL,
+  `reserved_at` int(10) UNSIGNED DEFAULT NULL,
+  `available_at` int(10) UNSIGNED NOT NULL,
+  `created_at` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -130,16 +157,16 @@ CREATE TABLE `jobs` (
 --
 
 CREATE TABLE `job_batches` (
-  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `total_jobs` int NOT NULL,
-  `pending_jobs` int NOT NULL,
-  `failed_jobs` int NOT NULL,
-  `failed_job_ids` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `options` mediumtext COLLATE utf8mb4_unicode_ci,
-  `cancelled_at` int DEFAULT NULL,
-  `created_at` int NOT NULL,
-  `finished_at` int DEFAULT NULL
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `total_jobs` int(11) NOT NULL,
+  `pending_jobs` int(11) NOT NULL,
+  `failed_jobs` int(11) NOT NULL,
+  `failed_job_ids` longtext NOT NULL,
+  `options` mediumtext DEFAULT NULL,
+  `cancelled_at` int(11) DEFAULT NULL,
+  `created_at` int(11) NOT NULL,
+  `finished_at` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -149,9 +176,9 @@ CREATE TABLE `job_batches` (
 --
 
 CREATE TABLE `migrations` (
-  `id` int UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int NOT NULL
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -168,7 +195,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (7, '2026_03_31_074449_create_periksa_table', 1),
 (8, '2026_03_31_074450_create_obat_table', 1),
 (9, '2026_03_31_074451_create_detail_periksa_table', 1),
-(10, '2026_03_31_074455_add_id_poli_to_user_table', 1);
+(10, '2026_03_31_074455_add_id_poli_to_user_table', 1),
+(11, '2026_04_23_045809_update_hari_in_jadwal_periksa_table', 2),
+(12, '2026_06_06_172047_add_stok_to_obat_table', 3);
 
 -- --------------------------------------------------------
 
@@ -177,13 +206,21 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 --
 
 CREATE TABLE `obat` (
-  `id` bigint UNSIGNED NOT NULL,
-  `nama_obat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kemasan` varchar(35) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `harga` int NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_obat` varchar(255) NOT NULL,
+  `kemasan` varchar(35) DEFAULT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL DEFAULT 0,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id`, `nama_obat`, `kemasan`, `harga`, `stok`, `created_at`, `updated_at`) VALUES
+(1, 'Bodrex', NULL, 7000, 0, '2026-04-14 09:13:08', '2026-06-06 12:25:25');
 
 -- --------------------------------------------------------
 
@@ -192,8 +229,8 @@ CREATE TABLE `obat` (
 --
 
 CREATE TABLE `password_reset_tokens` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -204,14 +241,22 @@ CREATE TABLE `password_reset_tokens` (
 --
 
 CREATE TABLE `periksa` (
-  `id` bigint UNSIGNED NOT NULL,
-  `id_daftar_poli` bigint UNSIGNED NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_daftar_poli` bigint(20) UNSIGNED NOT NULL,
   `tgl_periksa` datetime NOT NULL,
-  `catatan` text COLLATE utf8mb4_unicode_ci,
-  `biaya_periksa` int NOT NULL,
+  `catatan` text DEFAULT NULL,
+  `biaya_periksa` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `periksa`
+--
+
+INSERT INTO `periksa` (`id`, `id_daftar_poli`, `tgl_periksa`, `catatan`, `biaya_periksa`, `created_at`, `updated_at`) VALUES
+(1, 2, '2026-05-04 08:10:30', 'Cepat Sembuh Yak', 157000, '2026-05-04 01:10:30', '2026-05-04 01:10:30'),
+(2, 4, '2026-06-06 19:13:26', NULL, 157000, '2026-06-06 12:13:26', '2026-06-06 12:13:26');
 
 -- --------------------------------------------------------
 
@@ -220,12 +265,19 @@ CREATE TABLE `periksa` (
 --
 
 CREATE TABLE `poli` (
-  `id` bigint UNSIGNED NOT NULL,
-  `nama_poli` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `keterangan` text COLLATE utf8mb4_unicode_ci,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_poli` varchar(25) NOT NULL,
+  `keterangan` text DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `poli`
+--
+
+INSERT INTO `poli` (`id`, `nama_poli`, `keterangan`, `created_at`, `updated_at`) VALUES
+(2, 'Poli Mata', 'Poli Mata Baru', '2026-04-14 09:12:07', '2026-04-14 09:12:07');
 
 -- --------------------------------------------------------
 
@@ -234,13 +286,20 @@ CREATE TABLE `poli` (
 --
 
 CREATE TABLE `sessions` (
-  `id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_id` bigint UNSIGNED DEFAULT NULL,
-  `ip_address` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `user_agent` text COLLATE utf8mb4_unicode_ci,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_activity` int NOT NULL
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` longtext NOT NULL,
+  `last_activity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('GlAFg1Ievh5W1m7y9Z9szkzP0TQxKxXTppTpl4Pl', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoidUhpb3ZiNUtuREsxVWdjUXh2SnI4bVR3ZmRCcGlabXlDQVFTRHNVWCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1780774037);
 
 -- --------------------------------------------------------
 
@@ -249,20 +308,32 @@ CREATE TABLE `sessions` (
 --
 
 CREATE TABLE `users` (
-  `id` bigint UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `no_ktp` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `no_hp` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `no_rm` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `role` enum('admin','dokter','pasien') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `no_ktp` varchar(255) DEFAULT NULL,
+  `no_hp` varchar(255) DEFAULT NULL,
+  `no_rm` varchar(255) DEFAULT NULL,
+  `role` enum('admin','dokter','pasien') NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `id_poli` bigint UNSIGNED DEFAULT NULL
+  `id_poli` bigint(20) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `alamat`, `no_ktp`, `no_hp`, `no_rm`, `role`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `id_poli`) VALUES
+(1, 'Admin', NULL, NULL, NULL, NULL, 'admin', 'admin@gmail.com', '$2y$12$6nByNVTx2GN.8tBwI54rhOekUIFWEVC32xO6eN0PaplK5Z01A.OBO', NULL, '2026-04-05 23:35:24', '2026-04-05 23:35:24', NULL),
+(2, 'Dokter', 'Jl. Barokahh', '39943003033', '08363536633', NULL, 'dokter', 'dokter@gmail.com', '$2y$12$pavQ8pcc3NS9oIu5u/PqKeae9LIlULAAlufrRFKnj2jd6LxKf1MQm', NULL, '2026-04-05 23:35:24', '2026-04-22 22:23:07', 2),
+(3, 'Pasien', NULL, NULL, NULL, NULL, 'pasien', 'pasien@gmail.com', '$2y$12$YlrV1fk56MysKM6u4fiwlON9e3Mr2ywhXhrbYsU1e4eNV.PQiZqMi', NULL, '2026-04-05 23:35:24', '2026-04-05 23:35:24', NULL),
+(4, 'rafael', 'Jl. Hasanudin', '93475387834', '0845737534', '202604-004', 'pasien', 'rafael@test.com', '$2y$12$I2iMj6qyLXHukpsKsIXamONVIbxYZUz6VM0MJuuWz2BfWOucz/1DS', NULL, '2026-04-19 23:25:16', '2026-04-19 23:25:16', NULL),
+(5, 'rafael', 'Jl. Hasanudin', '43282482442', '0845343433', '202604-005', 'pasien', 'rafael@email.com', '$2y$12$1N3oHfPirJlYw306Ts78xuN44UDw6vnLByCchACtEtYuePcXLWERa', NULL, '2026-04-19 23:26:37', '2026-04-19 23:26:37', NULL),
+(6, 'miracle', 'Jl. Hasanudin', '1123432234324', '084583438340', '202604-006', 'pasien', 'miracle@gmail.com', '$2y$12$VZD/Ui7hMwdKLojozMcJtOokt.7oM4VfHj.lUW.K.8/IzgIfOr5vm', NULL, '2026-04-22 21:50:10', '2026-04-22 21:50:10', NULL);
 
 --
 -- Indexes for dumped tables
@@ -378,61 +449,61 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `daftar_poli`
 --
 ALTER TABLE `daftar_poli`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `detail_periksa`
 --
 ALTER TABLE `detail_periksa`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `jadwal_periksa`
 --
 ALTER TABLE `jadwal_periksa`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `obat`
 --
 ALTER TABLE `obat`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `periksa`
 --
 ALTER TABLE `periksa`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `poli`
 --
 ALTER TABLE `poli`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
